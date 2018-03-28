3fad1701ffc7597259e3e6d149d23cb99ac723d9
JonathanSimancaJs <jonathan.simanca@outlook.com>
2018-03-28 14:30:05 -0500

    Add broshure

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -60,6 +60,37 @@
         </div>
         <Footer/>
       </div>
+      <div class="conthex">
+        <div class="hex1"></div>
+        <div class="hex2"></div>
+        <div class="hex3"></div>
+        <div class="hex4"></div>
+        <div class="Text text-justify">
+          <h3>Habilidades</h3>
+          <div class="descbox">
+            <h1>MongoDB</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+               sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                 ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit
+                   esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa
+                     qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="descbox">
+            <h1>MongoDB</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+               sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                 ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit
+                   esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa
+                     qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -92,4 +123,156 @@ export default {
   overflow:hidden;
 }
 
+
+.conthex{
+  margin-top: 80px;
+  margin-left: 20%;
+  width: 800px;
+  height: 450px;
+  border: 1px solid;
+}
+.hex1{
+  background: red;
+  height: 100px;
+  width: 173.2px;
+  position: relative;
+  z-index: 1;
+}
+.hex1::after,
+.hex1::before{
+  content: "";
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  border-left: 86.6px solid transparent;
+  border-right: 86.6px solid transparent;
+}
+.hex1::after{
+  bottom: -100px;
+  border-top: 50px solid red;
+  border-bottom: 50px solid transparent;
+}
+.hex1::before{
+  top: -100px;
+  border-top: 50px solid transparent;
+  border-bottom: 50px solid red;
+}
+/*hooooo*/
+.hex2{
+  margin-top: 60px;
+  margin-left: 95px;
+  background: red;
+  height: 100px;
+  width: 173.2px;
+  position: relative;
+  z-index: 1;
+}
+.hex2::after,
+.hex2::before{
+  content: "";
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  border-left: 86.6px solid transparent;
+  border-right: 86.6px solid transparent;
+}
+.hex2::after{
+  bottom: -100px;
+  border-top: 50px solid red;
+  border-bottom: 50px solid transparent;
+}
+.hex2::before{
+  top: -100px;
+  border-top: 50px solid transparent;
+  border-bottom: 50px solid red;
+}
+
+/*hooooo*/
+.hex3{
+  margin-top: -100px;
+  margin-left: -95px;
+  background: red;
+  height: 100px;
+  width: 173.2px;
+  position: relative;
+  z-index: 1;
+}
+.hex3::after,
+.hex3::before{
+  content: "";
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  border-left: 86.6px solid transparent;
+  border-right: 86.6px solid transparent;
+}
+.hex3::after{
+  bottom: -100px;
+  border-top: 50px solid red;
+  border-bottom: 50px solid transparent;
+}
+.hex3::before{
+  top: -100px;
+  border-top: 50px solid transparent;
+  border-bottom: 50px solid red;
+}
+
+/*hooooo*/
+.hex4{
+  margin-top: 60px;
+  margin-left: 0px;
+  background: red;
+  height: 100px;
+  width: 173.2px;
+  position: relative;
+  z-index: 1;
+}
+.hex4::after,
+.hex4::before{
+  content: "";
+  position: absolute;
+  left: 0;
+  z-index: -1;
+  border-left: 86.6px solid transparent;
+  border-right: 86.6px solid transparent;
+}
+.hex4::after{
+  bottom: -100px;
+  border-top: 50px solid red;
+  border-bottom: 50px solid transparent;
+}
+.hex4::before{
+  top: -100px;
+  border-top: 50px solid transparent;
+  border-bottom: 50px solid red;
+}
+
+.Text{
+  margin-top: -350px;
+  padding-left: 300px;
+  padding-right: 20px;
+}
+
+.hex1:hover {
+  transform: scale(1.1);
+  transition-duration: 0.5s;
+}
+.hex2:hover {
+  transform: scale(1.1);
+  transition-duration: 0.5s;
+}
+.hex3:hover {
+  transform: scale(1.1);
+  transition-duration: 0.5s;
+}
+.hex4:hover {
+  transform: scale(1.1);
+  transition-duration: 0.5s;
+}
+.descbox{
+  display:none;
+}
+.descboxtrue{
+
+}
 </style>
